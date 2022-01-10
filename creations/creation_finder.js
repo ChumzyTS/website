8aa75c3f5815b3e7163c3216fc69f1f72d349fee
ChumzyTS <45547695+ChumzyTS@users.noreply.github.com>
2022-01-09 19:10:21 -0600

    Started on creation selector
    Currently it just loads data from .json files and creation a link based off that info.

--- creations/creation_finder.js
+++ creations/creation_finder.js
@@ -1,3 +1,86 @@
-jsonData = fetch('creations/creations.json')
-    .then(results => results.json())
-    .then(console.log);
+/*
+jsonData = null;
+awaitfetch('creations/creations.json')
+    .then((response) => {
+        jsonData = response.json();
+        return response.json();
+    })
+
+console.log(jsonData);
+*/
+
+// Async calls to get data that i hate
+async function getCreations() {
+    let url = 'creations/creations.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCreationInfo(name) {
+    let url = 'creations/creations/' + name + '/info.json';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function loadCreations() {
+    let data = await getCreations();
+    creations = data["creations"];
+    totalCreationNumber = creations.length;
+    creations.forEach(creation => {
+        console.log(addCreation(creation));
+    });
+}
+
+async function addCreation(name) {
+    let creationData = await getCreationInfo(name);
+    allCreationData.push(creationData);
+    creationsLoaded += 1;
+    listCreations();
+    return creationData;
+    console.log(creationData);
+}
+
+// a
+totalCreationNumber = -1;
+creationsLoaded = 0;
+allCreationData = [];
+function listCreations() {
+    if (creationsLoaded >= totalCreationNumber) {
+        console.log(allCreationData);
+        allCreationData.forEach(creation => {
+            link = document.createElement("a");
+
+            textNode = document.createTextNode(creation["name"]);
+            link.appendChild(textNode);
+
+            link.setAttribute("href",creation["link"]);
+
+            const holder = document.getElementById("creationHolder");
+            holder.appendChild(link);
+
+            breakElem = document.createElement("br");
+            holder.appendChild(breakElem);
+        });
+    }
+}
+
+loadCreations();
+
+/*
+var obj;
+
+fetch('creations/creations.json')
+  .then(res => res.json())
+  .then(data => obj = data)
+  .then(() => console.log(obj))
+
+console.log(obj);
+*/
